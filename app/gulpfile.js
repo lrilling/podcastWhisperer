2241944c3ab94f1ef4126a143018fc027207a003
Lukas Rilling <lukas.rilling@gmail.com>
2022-11-29 23:30:18 +0100

    Added a watcher to the task runner #15

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -7,6 +7,14 @@ const jest = require("gulp-jest");
 //         .pipe(jest.run({ json: false }, ['./src']));
 // })
 
+function build(cb) {
+    return gulp.src("./src/**/*.js")
+        .pipe(babel({
+            presets: ["@babel/preset-env"]
+        }))
+        .pipe(gulp.dest("./dist/"));
+}
+
 gulp.task("build", function () {
     return gulp.src("./src/**/*.js")
         .pipe(babel({
@@ -15,5 +23,8 @@ gulp.task("build", function () {
         .pipe(gulp.dest("./dist/"));
 });
 
-gulp.task("default", gulp.series("build"));
+// gulp.task("default", gulp.watch("./src/**/*.js", build));
+exports.default = function () {
+    gulp.watch("./src/**/*.js", build);
+}
 
